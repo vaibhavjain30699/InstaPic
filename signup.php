5af4a5edfa15d4bb29f68b83cc6bc1a9542cd484
Vaibhav Jain <vaibhavjain30699@gmail.com>
2018-12-22 14:27:48 +0530

    Added the Signup function

---
+++ signup.php
@@ -0,0 +1,36 @@
+<?php
+
+  $host = "localhost";
+  $user = "root";
+  $pass = "";
+  $db = "InstaPic";
+
+  $sql = mysqli_connect($host,$user,$pass,$db);
+
+  if($_POST['password']==$_POST['cpassword'])
+  {
+    $email = $_POST['email'];
+    $pass = $_POST['password'];
+    $query = "INSERT INTO login(id,email,password) VALUES(NULL,'".$email."','".$pass."')";
+    if(mysqli_query($sql,$query))
+    {
+      echo '<script language="javascript">';
+      echo 'alert("Registered Successfully")';
+      echo '</script>';
+      echo file_get_contents("index.html");
+    }
+    else {
+      echo '<script language="javascript">';
+      echo 'alert("Registration Unsuccessful")';
+      echo '</script>';
+      echo file_get_contents("signup.html");
+    }
+  }
+  else {
+    echo '<script language="javascript">';
+    echo 'alert("Confirm Password not equal to Password")';
+    echo '</script>';
+    echo file_get_contents("signup.html");
+  }
+
+ ?>
